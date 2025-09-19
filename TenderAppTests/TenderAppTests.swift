fade58a99916bde85c5043a48a3eee5b578fee1b
adriaaaann <akshvinthjohna@gmail.com>
2025-09-19 19:21:06 +0530

    final change

---
+++ TenderAppTests/TenderAppTests.swift
@@ -0,0 +1,16 @@
+//
+//  TenderAppTests.swift
+//  TenderAppTests
+//
+//  Created by Akshvinth 019 on 2025-09-19.
+//
+
+import Testing
+
+struct TenderAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
